482aec5177aa7167e734a5c5661d5065b18e568b
Luat <you@example.com>
2022-11-22 20:55:09 +0700

    Hi

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-	APP_ID: 'APP_ID',
-	REGION: 'REGION',
-	AUTH_KEY: 'AUTH_KEY'
+	APP_ID: '22604554a1bc8da9',
+	REGION: 'us',
+	AUTH_KEY: '602da0e1c8186ead3ceda9f4f4f0a11e0d318edf'
 }
